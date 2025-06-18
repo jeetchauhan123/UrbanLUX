635e76fdf2073194ac3ecb696b0a44ba31bf3f76
Jeet Chauhan <48512478+jeetchauhan123@users.noreply.github.com>
2025-06-18 20:46:47 +0530

    Update cloudinary.js

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -3,8 +3,6 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
   cloud_name: 'dgff46mle',
-  api_key: '999733331843512',
-  api_secret: '6BSVUF3oayYiINgUd7mb2Z8JZAQ',
 });
 
 const storage = new CloudinaryStorage({
