d9edc1b989ebbc6c5cb32304dfddb34cf9ef57bc
Jeet Chauhan <spgtjeet123@gmail.com>
2025-06-22 18:45:42 +0530

    Update PageNotFound.jsx

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -12,7 +12,7 @@ const PageNotFound = () => {
   return (
     <div className="nfoundbg flex flex-col items-center justify-center w-[100vw] h-[100vh] text-lg ">
       <div className="flex flex-col items-center justify-center bg-[#3a1051] px-10 py-25 gap-5 rounded-4xl shadow-[0_0_50px_-10px]">
-        <main className="flex flex-col items-center justify-center text-[#e7e7fc] font-bold">
+        <main className="flex flex-col items-center justify-center text-[#e7e7fc]">
           <p>Sorry people no page like this.</p>
           <p>would you like to return.</p>
         </main>
