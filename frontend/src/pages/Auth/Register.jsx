936013808e0d66a7cdd48facc422fde3f2651281
Jeet Chauhan <spgtjeet123@gmail.com>
2025-07-11 15:33:32 +0530

    Update Register.jsx

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -106,34 +106,34 @@ const Register = () => {
           <img
             src="../logo/dark-nobg.png"
             alt="darklogo"
-            className="brandlogo z-100 h-25 w-28 absolute top-4 left-5"
+            className="brandlogo z-100 h-20 w-23 sm:h-25 sm:w-28 absolute top-4 left-5 md:right-5 md:left-auto"
           />
         </Link>
 
-        <div className="w-[40%] h-[100vh] bg-[#3a1051]  shadow-[0_0_3rem] shadow-[#3a1051] absolute top-0 right-[15%] z-10 flex flex-col items-center justify-center">
-          <div className="flex flex-col items-center justify-center h-9/12 w-10/12">
+        <div className="w-[60vw] sm:w-[50vw] md:w-[40vw] h-[100vh] bg-[#3a1051]  shadow-[0_0_3rem] shadow-[#3a1051] absolute top-0 right-0 md:right-[15%] z-10 flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center h-10/12 w-10/12">
             <h1 className="text-3xl font-bold text-[#e7e7fc]">Registration</h1>
             <form
               className="flex flex-col items-center justify-center w-full h-full text-[#e7e7fc]"
               onSubmit={handleRegister}
             >
-              <p className="w-10/12 text-left">Enter Your Email:</p>
+              <p className="w-full sm:w-10/12 text-sm sm:text-base text-left">Enter Your Email:</p>
               <input
                 type="email"
                 placeholder="Email"
                 name="email"
                 onChange={(e) => setEmail(e.target.value)}
-                className="min-w-10/12 h-10 my-2 p-2 rounded-md outline-none border-2 border-[#e7e7fc]"
+                className="w-full sm:w-10/12 my-2 px-2 py-1 sm:p-2 rounded-md outline-none border-2 border-[#e7e7fc]"
               />
-              <p className="mx-auto w-10/12 text-left">Enter Your Password:</p>
+              <p className="mx-auto w-full sm:w-10/12 text-sm sm:text-base text-left">Enter Your Password:</p>
               <input
                 type="password"
                 placeholder="Password"
                 name="password"
                 onChange={(e) => setPassword(e.target.value)}
-                className="min-w-10/12 h-10 my-2 p-2 rounded-md outline-none border-2 border-[#e7e7fc]"
+                className="w-full sm:w-10/12 my-2 px-2 py-1 sm:p-2 rounded-md outline-none border-2 border-[#e7e7fc]"
               />
-              <p className="mx-auto w-10/12 text-left">
+              <p className="mx-auto w-full sm:w-10/12 text-sm sm:text-base text-left">
                 Confirm Your Password:
               </p>
               <input
@@ -141,7 +141,7 @@ const Register = () => {
                 placeholder="Confirm Password"
                 name="confirmPassword"
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                className="min-w-10/12 h-10 my-2 p-2 rounded-md outline-none border-2 border-[#e7e7fc]"
+                className="w-full sm:w-10/12 my-2 px-2 py-1 sm:p-2 rounded-md outline-none border-2 border-[#e7e7fc]"
               />
               {/* Error message */}
               {error && (
@@ -151,12 +151,12 @@ const Register = () => {
               )}
               <button
                 type="submit"
-                className="loginbtn w-10/12 h-10 my-2 bg-[#e7e7fc] text-[#3a1051] font-bold rounded-md cursor-pointer"
+                className="loginbtn w-full sm:w-10/12 py-2 my-2 bg-[#e7e7fc] text-[#3a1051] font-bold rounded-md cursor-pointer text-sm sm:text-base"
               >
                 Register
               </button>
 
-              <p className="text-[#e7e7fc]">
+              <p className="w-full sm:w-10/12 text-sm sm:text-base text-left text-[#e7e7fc]">
                 Already have account?{" "}
                 <Link
                   className="text-blue-500 cursor-pointer hover:underline"
@@ -167,32 +167,33 @@ const Register = () => {
               </p>
             </form>
 
-            <div>
+            {/* company login */}
+            <div className="w-full">
               <p className="text-[#e7e7fc] text-center mt-4">Or login with:</p>
-              <div className="flex items-center justify-center mt-2 text-[#e7e7fc] font-medium gap-5">
-                <button className="h-18 w-20 bg-white flex flex-col items-center justify-center text-[#3a1051] rounded-md cursor-pointer">
+              <div className="flex flex-col md:flex-row items-center justify-center mt-2 text-[#e7e7fc] font-medium gap-2 md:gap-5">
+                <button className="h-8 w-full sm:w-10/12 md:h-18 md:w-20 bg-white flex flex-row md:flex-col items-center justify-center text-[#3a1051] rounded-md cursor-pointer text-sm sm:text-base">
                   <img
                     src="../google-logo.svg"
                     alt="google"
-                    className="h-8 w-8 mx-2 "
+                    className="h-5 w-5 md:h-8 md:w-8 mx-2"
                   />
                   Google
                 </button>
 
-                <button className="h-18 w-20 bg-[#1877F2] flex flex-col items-center justify-center rounded-md cursor-pointer">
+                <button className="h-8 w-full sm:w-10/12 md:h-18 md:w-20 bg-[#1877F2] flex flex-row md:flex-col items-center justify-center rounded-md cursor-pointer text-sm sm:text-base">
                   <img
                     src="../facebook-logo.svg"
                     alt="facebook"
-                    className="h-8 w-8 mx-2 "
+                    className="h-5 w-5 md:h-8 md:w-8 mx-2"
                   />
                   Facebook
                 </button>
 
-                <button className="h-18 w-20 bg-black flex flex-col items-center justify-center rounded-md cursor-pointer">
+                <button className="h-8 w-full sm:w-10/12 md:h-18 md:w-20 bg-black flex flex-row md:flex-col items-center justify-center rounded-md cursor-pointer text-sm sm:text-base">
                   <img
                     src="../github-black.svg"
                     alt="github"
-                    className="h-8 w-8 mx-2  bg-white rounded-[50%]"
+                    className="h-5 w-5 md:h-8 md:w-8 mx-2 bg-white rounded-[50%]"
                   />
                   GitHub
                 </button>
